af7dcf65c837aa9cebc69c124844640a8351a5d1
Frank Rosner <frank@fam-rosner.de>
2019-04-21 08:58:03 +0200

    First test case (success after retry)

--- src/main/kotlin/de/frosner/vkre/FraudCheckService.kt
+++ src/main/kotlin/de/frosner/vkre/FraudCheckService.kt
@@ -2,10 +2,14 @@ package de.frosner.vkre
 
 import io.vertx.circuitbreaker.CircuitBreaker
 import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import io.vertx.ext.web.client.HttpResponse
 import io.vertx.ext.web.client.WebClient
 import io.vertx.kotlin.circuitbreaker.executeCommandAwait
+import io.vertx.kotlin.circuitbreaker.executeCommandWithFallbackAwait
 import io.vertx.kotlin.coroutines.dispatcher
 import io.vertx.kotlin.ext.web.client.sendAwait
+import java.util.function.Function
 
 class FraudCheckService(
     private val vertx: Vertx,
@@ -14,10 +18,35 @@ class FraudCheckService(
 ) {
 
     private val client: WebClient = WebClient.create(vertx)
+    private val handlerFactory = CoroutineHandlerFactory(vertx.dispatcher())
 
     suspend fun checkFraud(): Boolean {
+        return circuitBreaker.executeCommandAwait(
+            command = handlerFactory.create(
+                handler = {
+                    print("Requesting...")
+                    val response = client.getAbs(apiUrl).sendAwait()
+                    Pair<HttpResponse<Buffer>, Boolean>(response, response.bodyAsString().toBoolean())
+                },
+                failure = {
+                    when (it.statusCode()) {
+                        200 -> {
+                            println(" Success (200)")
+                            null
+                        }
+                        else -> {
+                            println(" Failure (${it.statusCode()})")
+                            "Status code was ${it.statusCode()}"
+                        }
+                    }
+                }
+            )
+        )
+    }
+
+    suspend fun checkFraudWithFallback(totalPrice: Int): Boolean {
         val handlerFactory = CoroutineHandlerFactory(vertx.dispatcher())
-        val response = circuitBreaker.executeCommandAwait(
+        val response = circuitBreaker.executeCommandWithFallbackAwait(
             command = handlerFactory.create(
                 handler = {
                     print("Requesting...")
@@ -36,7 +65,10 @@ class FraudCheckService(
                         }
                     }
                 }
-            )
+            ),
+            fallback = Function {
+                totalPrice > 100
+            }
         )
         return response
     }
--- src/main/kotlin/de/frosner/vkre/Main.kt
+++ src/main/kotlin/de/frosner/vkre/Main.kt
@@ -4,7 +4,19 @@ import io.vertx.circuitbreaker.CircuitBreaker
 import io.vertx.core.Vertx
 import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.circuitbreaker.circuitBreakerOptionsOf
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import java.util.*
+
+/*
+ * CircuitBreakerOptions
+ *
+ * @param fallbackOnFailure  Sets whether or not the fallback is executed on failure, even when the circuit is closed.
+ * @param maxFailures  Sets the maximum number of failures before opening the circuit.
+ * @param maxRetries  Configures the number of times the circuit breaker tries to redo the operation before failing.
+ * @param resetTimeout  Sets the time in ms before it attempts to re-close the circuit (by going to the half-open state). If the circuit is closed when the timeout is reached, nothing happens. <code>-1</code> disables this feature.
+ * @param timeout  Sets the timeout in milliseconds. If an action is not completed before this timeout, the action is considered as a failure.
+ */
 
 object Main {
     suspend fun <T> tryOrPrint(f: suspend () -> T): Unit = try {
@@ -13,45 +25,66 @@ object Main {
         println(t)
     }
 
+    suspend fun successAfterRetry(vertx: Vertx) {
+        val responses: List<(RoutingContext) -> Unit> = listOf(
+            { ctx -> ctx.response().setStatusCode(500).end() },
+            { ctx -> ctx.response().setStatusCode(200).end("false") }
+        )
+        val server = FraudCheckApi(vertx, responses)
+        val serverPort = server.start()
+
+        val options = circuitBreakerOptionsOf(
+            maxRetries = 1
+        )
+
+        val circuitBreaker = CircuitBreaker.create(UUID.randomUUID().toString(), vertx, options)
+        val client = FraudCheckService(vertx, circuitBreaker, "http://localhost:$serverPort/")
+
+        tryOrPrint { client.checkFraud() }
+    }
+
+    suspend fun test(vertx: Vertx) {
+        val responses: List<(RoutingContext) -> Unit> = listOf(
+            { ctx -> ctx.response().setStatusCode(500).end() },
+            { ctx -> println(" Timing out") },
+            { ctx -> println(" Timing out") },
+            { ctx -> ctx.response().setStatusCode(200).end("true") }
+        )
+        val server = FraudCheckApi(vertx, responses)
+        val serverPort = server.start()
+
+
+        /*
+         * @param fallbackOnFailure  Sets whether or not the fallback is executed on failure, even when the circuit is closed.
+         * @param maxFailures  Sets the maximum number of failures before opening the circuit.
+         * @param maxRetries  Configures the number of times the circuit breaker tries to redo the operation before failing.
+         * @param resetTimeout  Sets the time in ms before it attempts to re-close the circuit (by going to the half-open state). If the circuit is closed when the timeout is reached, nothing happens. <code>-1</code> disables this feature.
+         * @param timeout  Sets the timeout in milliseconds. If an action is not completed before this timeout, the action is considered as a failure.
+         */
+        val options = circuitBreakerOptionsOf(
+            fallbackOnFailure = false,
+            maxFailures = 1,
+            maxRetries = 2,
+            resetTimeout = 5000,
+            timeout = 2000
+        )
+
+        val circuitBreaker = CircuitBreaker.create("my-circuit-breaker", vertx, options)
+        val client = FraudCheckService(vertx, circuitBreaker, "http://localhost:$serverPort/")
+
+        tryOrPrint { client.checkFraud() }
+        tryOrPrint { client.checkFraud() }
+        println("Waiting for circuit to be open again...")
+        delay(6000)
+        tryOrPrint { client.checkFraud() }
+    }
+
     @JvmStatic
     fun main(args: Array<String>) {
         runBlocking {
             val vertx = Vertx.vertx()
-
             try {
-                val responses: List<(RoutingContext) -> Unit> = listOf(
-                    { ctx -> ctx.response().setStatusCode(500).end() },
-                    { ctx -> println(" Timing out") },
-                    { ctx -> println(" Timing out") },
-                    { ctx -> ctx.response().setStatusCode(200).end("true") }
-                )
-                val server = FraudCheckApi(vertx, responses)
-                val serverPort = server.start()
-
-
-                /*
-                 * @param fallbackOnFailure  Sets whether or not the fallback is executed on failure, even when the circuit is closed.
-                 * @param maxFailures  Sets the maximum number of failures before opening the circuit.
-                 * @param maxRetries  Configures the number of times the circuit breaker tries to redo the operation before failing.
-                 * @param resetTimeout  Sets the time in ms before it attempts to re-close the circuit (by going to the half-open state). If the circuit is closed when the timeout is reached, nothing happens. <code>-1</code> disables this feature.
-                 * @param timeout  Sets the timeout in milliseconds. If an action is not completed before this timeout, the action is considered as a failure.
-                 */
-                val options = circuitBreakerOptionsOf(
-                    fallbackOnFailure = false,
-                    maxFailures = 1,
-                    maxRetries = 2,
-                    resetTimeout = 5000,
-                    timeout = 2000
-                )
-
-                val circuitBreaker = CircuitBreaker.create("my-circuit-breaker", vertx, options)
-                val client = FraudCheckService(vertx, circuitBreaker, "http://localhost:$serverPort/")
-
-                tryOrPrint { client.checkFraud() }
-                tryOrPrint { client.checkFraud() }
-                println("Waiting for circuit to be open again...")
-                Thread.sleep(6000)
-                tryOrPrint { client.checkFraud() }
+                successAfterRetry(vertx)
             } finally {
                 vertx.close()
             }
